d82b35fe18c51d353972e540c1cafbbaa940f0e5
Chris Alfano <chris@jarv.us>
2018-12-07 05:38:39 +0000

    feat: add config plan for nginx

---
+++ plan.sh
@@ -0,0 +1,30 @@
+pkg_name=multihost-nginx
+pkg_origin=emergence
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=('MIT')
+pkg_version=1.0.0
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+pkg_svc_run="nginx -c ${pkg_svc_config_path}/nginx.conf"
+pkg_deps=(
+  core/node
+  core/nginx
+)
+
+do_build() {
+  pushd "${PLAN_CONTEXT}" > /dev/null
+  cp "build-config" "package.json" "package-lock.json" "${CACHE_PATH}/"
+  popd > /dev/null
+
+
+  pushd "${CACHE_PATH}" > /dev/null
+  build_line "Installing dependencies with NPM"
+  npm install
+  popd > /dev/null
+}
+
+do_install() {
+  pushd "${CACHE_PATH}" > /dev/null
+  cp -r ./* "${pkg_prefix}/"
+  popd > /dev/null
+}
